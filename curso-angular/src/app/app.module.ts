81a7a2fae1236b1fc37c28c671933d2c0a02baed
Heflain da Rocha Mendes <58933568+heflain-mendes@users.noreply.github.com>
2022-11-05 22:38:08 -0300

    aula 04

--- curso-angular/src/app/app.module.ts
+++ curso-angular/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FristComponentComponent } from './components/frist-component/frist-component.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FristComponentComponent
   ],
   imports: [
     BrowserModule,
