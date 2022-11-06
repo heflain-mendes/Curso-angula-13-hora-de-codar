d4853f5726f4335b37513fd71ee313a537f845e8
Heflain da Rocha Mendes <58933568+heflain-mendes@users.noreply.github.com>
2022-11-06 16:25:36 -0300

    aula 11

--- curso-angular/src/app/components/emitter/emitter.component.ts
+++ curso-angular/src/app/components/emitter/emitter.component.ts
@@ -7,9 +7,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EmitterComponent implements OnInit {
 
+  myNumber : number = 0;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  onChangeNumber() :void{
+    this.myNumber = Math.floor(Math.random() * 10) + 1;
+  }
+
+  onResetNumber() : void{
+    this.myNumber = -1;
   }
 
 }
